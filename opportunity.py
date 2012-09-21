618230abaf5dd769732ac6628309e43df52dbf1f
Shalabh Aggarwal <shalabh.aggarwal@openlabs.co.in>
2012-09-21 19:03:41 +0530

    More URLs and methods

--- opportunity.py
+++ opportunity.py
@@ -8,7 +8,8 @@
     :license: GPLv3, see LICENSE for more details.
 """
 from nereid import (request, abort, render_template, login_required, url_for,
-    redirect, flash, jsonify)
+    redirect, flash, jsonify, permissions_required)
+from nereid.contrib.pagination import Pagination
 from trytond.model import ModelView, ModelSQL, ModelSingleton, Workflow, fields
 from trytond.pool import Pool
 from trytond.pyson import Eval
@@ -30,6 +31,10 @@ class SaleOpportunity(Workflow, ModelSQL, ModelView):
     _name = "sale.opportunity"
 
     ip_address = fields.Char('IP Address')
+    reviews = fields.One2Many(
+        'nereid.review',
+        'lead', 'Reviews'
+    )
 
     def new_opportunity(self):
         """Create a new sale opportunity
@@ -103,6 +108,84 @@ class SaleOpportunity(Workflow, ModelSQL, ModelView):
         "A thanks template rendered"
         return render_template('crm/thanks.jinja')
 
+    @login_required
+    @permissions_required(['sales.admin'])
+    def sales_home(self):
+        """
+        Shows a home page for the sale opportunities
+        """
+        return render_template('crm/home.jinja')
+
+    @login_required
+    @permissions_required(['sales.admin'])
+    def all_leads(self, page=1):
+        """
+        All leads captured
+        """
+        filter_domain = []
+
+        company = request.args.get('company', None)
+        if company:
+            filter_domain.append(('party.name', 'ilike', '%%%s%%' % company))
+
+        name = request.args.get('name', None)
+        if name:
+            filter_domain.append(
+                ('address.name', 'ilike', '%%%s%%' % name)
+            )
+
+        email = request.args.get('email', None)
+        if email:
+            filter_domain.append(
+                ('address.email', 'ilike', '%%%s%%' % email)
+            )
+
+        state = request.args.get('state', None)
+        if state:
+            filter_domain.append(
+                ('state', '=', '%s' % state)
+            )
+
+        leads = Pagination(self, filter_domain, page, 10)
+        return render_template(
+            'crm/leads.jinja', leads=leads
+        )
+
+    @login_required
+    @permissions_required(['sales.admin'])
+    def admin_lead(self, id):
+        """
+        Lead
+        """
+        lead = self.browse(id)
+        return render_template(
+            'crm/admin-lead.jinja', lead=lead
+        )
+
+    @login_required
+    @permissions_required(['sales.admin'])
+    def add_review(self):
+        """Add a review for the lead
+        """
+        review_obj = Pool().get('nereid.review')
+        lead_id = request.form.get('lead', type=int)
+
+        lead = self.browse(lead_id)
+
+        review_obj.create({
+            'lead': lead.id,
+            'title': request.form.get('title'),
+            'comment': request.form.get('comment'),
+            'nereid_user': request.nereid_user.id,
+            'party': lead.party.id,
+        })
+        if request.is_xhr:
+            return jsonify({
+                'success': True,
+                'message': 'The review has been added.'
+            })
+        return redirect(request.referrer + '#review')
+
 SaleOpportunity()
 
 
@@ -135,3 +218,17 @@ class CompanySalesTeam(ModelSQL):
     )
 
 CompanySalesTeam()
+
+
+class NereidReview(ModelSQL, ModelView):
+    """
+    Nereid Review
+    """
+    _name = "nereid.review"
+
+    lead = fields.Many2One(
+        'sale.opportunity', 'Sale Opportunity Lead'
+    )
+
+NereidReview()
+
