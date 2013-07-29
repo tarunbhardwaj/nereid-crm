08940672e3c1a669559c5d70f6de76b4a1d88022
Tarun Bhardwaj <tarun.bhardwaj@openlabs.co.in>
2013-07-29 14:24:32 +0530

    Migration to 2.6 with test case #1899
    Migrate nereid_crm to 2.6 and test cases
    
    Review ID: 304001

--- opportunity.py
+++ opportunity.py
@@ -4,12 +4,12 @@
 
     Mini CRM based on Nereid and Sale Opprotunity
 
-    :copyright: (c) 2012 by Openlabs Technologies & Consulting (P) Limited
+    :copyright: (c) 2012-2013 by Openlabs Technologies & Consulting (P) Limited
     :license: GPLv3, see LICENSE for more details.
 """
 from decimal import Decimal
 import logging
-from wtforms import (Form, IntegerField, TextField, SelectField, TextAreaField, 
+from wtforms import (Form, IntegerField, TextField, SelectField, TextAreaField,
     validators)
 from wtfrecaptcha.fields import RecaptchaField
 
@@ -17,10 +17,17 @@ from nereid import (request, abort, render_template, login_required, url_for,
     redirect, flash, jsonify, permissions_required, render_email)
 from nereid.contrib.pagination import Pagination
 from trytond.model import ModelView, ModelSQL, ModelSingleton, Workflow, fields
-from trytond.pool import Pool
+from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval
 from trytond.config import CONFIG
 from trytond.tools import get_smtp_server
+
+
+__all__ = [
+    'Configuration', 'NereidReview', 'CompanySalesTeam', 'Company',
+    'SaleOpportunity', 'NereidUser',
+]
+__metaclass__ = PoolMeta
 
 geoip = None
 try:
@@ -41,15 +48,26 @@ else:
             pass
 
 
-class Configuration(ModelSingleton, ModelSQL, ModelView):
+class NereidUser:
+    """
+    Add employee
+    """
+    __name__ = "nereid.user"
+
+    #: Allow the nereid user to be connected to an internal employee. This
+    #: indicates that the user is an employee and not a regular participant
+    employee = fields.Many2One(
+        'company.employee', 'Employee', select=True,
+    )
+
+
+class Configuration:
     "Sale Opportunity configuration"
-    _name = 'sale.configuration'
+    __name__ = 'sale.configuration'
 
     website_employee = fields.Property(
         fields.Many2One('company.employee', 'Website Employee')
     )
-
-Configuration()
 
 
 class ContactUsForm(Form):
@@ -67,9 +85,9 @@ class ContactUsForm(Form):
     comment = TextAreaField('Comment', [validators.Required(),])
 
 
-class SaleOpportunity(Workflow, ModelSQL, ModelView):
+class SaleOpportunity:
     "Sale Opportunity"
-    _name = "sale.opportunity"
+    __name__ = "sale.opportunity"
 
     ip_address = fields.Char('IP Address')
     reviews = fields.One2Many(
@@ -80,38 +98,37 @@ class SaleOpportunity(Workflow, ModelSQL, ModelView):
 
     contactus_form = ContactUsForm
 
-    def new_opportunity(self):
-        """Create a new sale opportunity
+    @classmethod
+    def new_opportunity(cls):
         """
-        country_obj = Pool().get('country.country')
+        Web handler to create a new sale opportunity
+        """
+        Country = Pool().get('country.country')
 
         if 're_captcha_public' in CONFIG.options:
-            contact_form = self.contactus_form(
+            contact_form = cls.contactus_form(
                 request.form,
                 captcha={'ip_address': request.remote_addr}
             )
         else:
-            contact_form = self.contactus_form(request.form)
+            contact_form = cls.contactus_form(request.form)
 
-        country_ids = country_obj.search([])
-        countries = country_obj.browse(country_ids)
+        countries = Country.search([])
 
         contact_form.country.choices = [
             (c.id, c.name) for c in countries
         ]
 
         if request.method == 'POST' and contact_form.validate():
-            address_obj = Pool().get('party.address')
-            contact_mech_obj = Pool().get('party.contact_mechanism')
-            party_obj = Pool().get('party.party')
-            config_obj = Pool().get('sale.configuration')
-            company_obj = Pool().get('company.company')
-            country_obj = Pool().get('country.country')
+            Address = Pool().get('party.address')
+            ContactMech = Pool().get('party.contact_mechanism')
+            Party = Pool().get('party.party')
+            Config = Pool().get('sale.configuration')
+            Company = Pool().get('company.company')
+            Country = Pool().get('country.country')
 
-            config = config_obj.browse(1)
-
+            config = Config(1)
             contact_data = contact_form.data
-
             # Create Party
             company = request.nereid_website.company.id
 
@@ -122,7 +139,7 @@ class SaleOpportunity(Workflow, ModelSQL, ModelView):
             else:
                 detected_country = ''
 
-            party_id = party_obj.create({
+            party = Party.create({
                 'name': contact_data.get('company') or \
                     contact_data['name'],
                 'addresses': [
@@ -131,11 +148,10 @@ class SaleOpportunity(Workflow, ModelSQL, ModelView):
                         'country': contact_data['country'],
                         })],
                 })
-            party = party_obj.browse(party_id)
 
             if contact_data.get('website'):
                 # Create website as contact mech
-                contact_mech_id = contact_mech_obj.create({
+                contact_mech = ContactMech.create({
                         'type': 'website',
                         'party': party.id,
                         'website': contact_data['website'],
@@ -143,22 +159,24 @@ class SaleOpportunity(Workflow, ModelSQL, ModelView):
 
             if contact_data.get('phone'):
                 # Create phone as contact mech and assign as phone
-                contact_mech_id = contact_mech_obj.create({
+                contact_mech = ContactMech.create({
                         'type': 'phone',
                         'party': party.id,
                         'other_value': contact_data['phone'],
                     })
-                address_obj.write(party.addresses[0].id,
-                    {'phone': contact_data['phone']})
+                Address.write(
+                    [party.addresses[0]], {'phone': contact_data['phone']}
+                )
 
             # Create email as contact mech and assign as email
-            contact_mech_id = contact_mech_obj.create({
+            contact_mech = ContactMech.create({
                     'type': 'email',
                     'party': party.id,
                     'email': contact_data['email'],
                 })
-            address_obj.write(party.addresses[0].id,
-                {'email': contact_data['email']})
+            Address.write(
+                [party.addresses[0]], {'email': contact_data['email']}
+            )
 
             # Create sale opportunity
             if request.nereid_user.employee:
@@ -170,34 +188,33 @@ class SaleOpportunity(Workflow, ModelSQL, ModelView):
                 description =  'Created from website'
             employee = request.nereid_user.employee.id \
                 if request.nereid_user.employee else config.website_employee.id
-            lead_id = self.create({
-                    'party': party_id,
-                    'company': company,
-                    'employee': employee,
-                    'address': party.addresses[0].id,
-                    'description': description,
-                    'comment': contact_data['comment'],
-                    'ip_address': request.remote_addr,
-                    'detected_country': detected_country,
-                })
-            self.send_notification_mail(lead_id)
+            lead = cls.create({
+                'party': party.id,
+                'company': company,
+                'employee': employee,
+                'address': party.addresses[0].id,
+                'description': description,
+                'comment': contact_data['comment'],
+                'ip_address': request.remote_addr,
+                'detected_country': detected_country,
+            })
+            lead.send_notification_mail()
 
             return redirect(request.args.get('next',
-                url_for('sale.opportunity.admin_lead', id=lead_id)))
+                url_for('sale.opportunity.admin_lead', active_id=lead.id)))
         return render_template('crm/sale_form.jinja', form=contact_form)
 
-    def send_notification_mail(self, lead_id):
-        """Send a notification mail to sales department whenever there is query
+    def send_notification_mail(self):
+        """
+        Send a notification mail to sales department whenever there is query
         for new lead.
 
         :param lead_id: ID of lead.
         """
-        lead = self.browse(lead_id)
-
         # Prepare the content for email.
-        subject = "[Openlabs CRM] New lead created by %s" % (lead.party.name)
+        subject = "[Openlabs CRM] New lead created by %s" % (self.party.name)
 
-        receivers = [member.email for member in lead.company.sales_team
+        receivers = [member.email for member in self.company.sales_team
                      if member.email]
         if not receivers:
             return
@@ -207,7 +224,7 @@ class SaleOpportunity(Workflow, ModelSQL, ModelView):
             to=', '.join(receivers),
             subject=subject,
             text_template='crm/emails/notification_text.jinja',
-            lead=lead
+            lead=self
         )
 
         # Send mail.
@@ -217,88 +234,86 @@ class SaleOpportunity(Workflow, ModelSQL, ModelView):
         )
         server.quit()
 
-    def new_opportunity_thanks(self):
+    @classmethod
+    def new_opportunity_thanks(cls):
         "A thanks template rendered"
         return render_template('crm/thanks.jinja')
 
     @login_required
     @permissions_required(['sales.admin'])
-    def revenue_opportunity(self, lead_id):
-        """Set the Conversion Probability and estimated revenue amount
+    def revenue_opportunity(self):
         """
-        nereid_user_obj = Pool().get('nereid.user')
-        lead  = self.browse(lead_id)
+        Set the Conversion Probability and estimated revenue amount
+        """
+        NereidUser = Pool().get('nereid.user')
 
-        nereid_user_id = nereid_user_obj.search(
-            [('employee', '=', lead.employee.id)], limit=1
+        nereid_user = NereidUser.search(
+            [('employee', '=', self.employee.id)], limit=1
         )
-        if nereid_user_id:
-            employee = nereid_user_obj.browse(nereid_user_id[0])
+        if nereid_user:
+            employee = nereid_user[0]
         else:
             employee = None
 
         if request.method == 'POST':
-            self.write(lead.id, {
+            self.write([self], {
                 'probability': request.form['probability'],
                 'amount': Decimal(request.form.get('amount'))
             })
             flash('Lead has been updated.')
             return redirect(url_for(
-                'sale.opportunity.admin_lead', id=lead.id) + "#tab-revenue"
+                'sale.opportunity.admin_lead', active_id=self.id) + "#tab-revenue"
             )
         return render_template(
-            'crm/admin-lead.jinja', lead=lead, employee=employee,
+            'crm/admin-lead.jinja', lead=self, employee=employee,
         )
 
+    @classmethod
     @login_required
     @permissions_required(['sales.admin'])
-    def sales_home(self):
+    def sales_home(cls):
         """
         Shows a home page for the sale opportunities
         """
-        country_obj = Pool().get('country.country')
+        Country = Pool().get('country.country')
 
-        country_ids = country_obj.search([])
-        countries = country_obj.browse(country_ids)
+        countries = Country.search([])
 
         counter = {}
         for state in ('lead', 'opportunity', 'converted', 'cancelled', 'lost'):
-            counter[state] = self.search([('state', '=', state)], count=True)
+            counter[state] = cls.search([('state', '=', state)], count=True)
         return render_template(
             'crm/home.jinja', counter=counter, countries=countries
         )
 
     @login_required
     @permissions_required(['sales.admin'])
-    def assign_lead(self, lead_id):
+    def assign_lead(self):
         "Change the employee on lead"
-        lead = self.browse(lead_id)
-        nereid_user_obj = Pool().get('nereid.user')
+        NereidUser = Pool().get('nereid.user')
 
-        new_assignee = nereid_user_obj.browse(int(request.form['user']))
-
-        if lead.employee.id == new_assignee.id:
+        new_assignee = NereidUser(int(request.form['user']))
+        if self.employee.id == new_assignee.employee.id:
             flash("Lead already assigned to %s" % new_assignee.name)
             return redirect(request.referrer)
 
-        self.write(lead.id, {
+        self.write([self], {
             'employee': new_assignee.employee.id
         })
 
         flash("Lead assigned to %s" % new_assignee.name)
         return redirect(request.referrer)
 
+    @classmethod
     @login_required
     @permissions_required(['sales.admin'])
-    def all_leads(self, page=1):
+    def all_leads(cls, page=1):
         """
         All leads captured
         """
-        country_obj = Pool().get('country.country')
+        Country = Pool().get('country.country')
 
-        country_ids = country_obj.search([])
-        countries = country_obj.browse(country_ids)
-
+        countries = Country.search([])
         filter_domain = []
 
         company = request.args.get('company', None)
@@ -323,47 +338,45 @@ class SaleOpportunity(Workflow, ModelSQL, ModelView):
                 ('state', '=', '%s' % state)
             )
 
-        leads = Pagination(self, filter_domain, page, 10)
+        leads = Pagination(cls, filter_domain, page, 10)
         return render_template(
             'crm/leads.jinja', leads=leads, countries=countries
         )
 
     @login_required
     @permissions_required(['sales.admin'])
-    def admin_lead(self, id):
+    def admin_lead(self):
         """
         Lead
         """
-        nereid_user_obj = Pool().get('nereid.user')
-        country_obj = Pool().get('country.country')
+        NereidUser = Pool().get('nereid.user')
+        Country = Pool().get('country.country')
 
-        country_ids = country_obj.search([])
-        countries = country_obj.browse(country_ids)
-
-        lead = self.browse(id)
-        nereid_user_id = nereid_user_obj.search(
-            [('employee', '=', lead.employee.id)], limit=1
+        countries = Country.search([])
+        nereid_users = NereidUser.search(
+            [('employee', '=', self.employee.id)], limit=1
         )
-        if nereid_user_id:
-            employee = nereid_user_obj.browse(nereid_user_id[0])
+        if nereid_users:
+            employee = nereid_users[0]
         else:
             employee = None
         return render_template(
-            'crm/admin-lead.jinja', lead=lead, employee=employee,
+            'crm/admin-lead.jinja', lead=self, employee=employee,
             countries=countries
         )
 
+    @classmethod
     @login_required
     @permissions_required(['sales.admin'])
-    def add_comment(self):
-        """Add a comment for the lead
+    def add_comment(cls):
         """
-        review_obj = Pool().get('nereid.review')
+        Add a comment for the lead
+        """
+        Review = Pool().get('nereid.review')
         lead_id = request.form.get('lead', type=int)
+        lead = cls(lead_id)
 
-        lead = self.browse(lead_id)
-
-        review_obj.create({
+        Review.create({
             'lead': lead.id,
             'title': request.form.get('title'),
             'comment': request.form.get('comment'),
@@ -379,10 +392,11 @@ class SaleOpportunity(Workflow, ModelSQL, ModelView):
 
     @login_required
     @permissions_required(['sales.admin'])
-    def mark_opportunity(self, lead_id):
-        """Convert the lead to opportunity
+    def mark_opportunity(self):
         """
-        self.opportunity([lead_id])
+        Convert the lead to opportunity
+        """
+        self.opportunity([self])
         if request.is_xhr:
             return jsonify({
                 'success': True,
@@ -392,10 +406,11 @@ class SaleOpportunity(Workflow, ModelSQL, ModelView):
 
     @login_required
     @permissions_required(['sales.admin'])
-    def mark_lost(self, lead_id):
-        """Convert the lead to lost
+    def mark_lost(self):
         """
-        self.lost([lead_id])
+        Convert the lead to lost
+        """
+        self.lost([self])
         if request.is_xhr:
             return jsonify({
                 'success': True,
@@ -405,10 +420,11 @@ class SaleOpportunity(Workflow, ModelSQL, ModelView):
 
     @login_required
     @permissions_required(['sales.admin'])
-    def mark_lead(self, lead_id):
-        """Convert the opportunity to lead
+    def mark_lead(self):
         """
-        self.lead([lead_id])
+        Convert the opportunity to lead
+        """
+        self.lead([self])
         if request.is_xhr:
             return jsonify({
                 'success': True,
@@ -418,10 +434,11 @@ class SaleOpportunity(Workflow, ModelSQL, ModelView):
 
     @login_required
     @permissions_required(['sales.admin'])
-    def mark_converted(self, lead_id):
-        """Convert the opportunity
+    def mark_converted(self):
         """
-        self.convert([lead_id])
+        Convert the opportunity
+        """
+        self.convert([self])
         if request.is_xhr:
             return jsonify({
                 'success': True,
@@ -431,10 +448,11 @@ class SaleOpportunity(Workflow, ModelSQL, ModelView):
 
     @login_required
     @permissions_required(['sales.admin'])
-    def mark_cancelled(self, lead_id):
-        """Convert the lead as cancelled
+    def mark_cancelled(self):
         """
-        self.cancel([lead_id])
+        Convert the lead as cancelled
+        """
+        self.cancel([self])
         if request.is_xhr:
             return jsonify({
                 'success': True,
@@ -442,26 +460,22 @@ class SaleOpportunity(Workflow, ModelSQL, ModelView):
             })
         return redirect(request.referrer)
 
-SaleOpportunity()
 
 
-class Company(ModelSQL, ModelView):
+class Company:
     "Company"
-    _name = 'company.company'
+    __name__ = 'company.company'
 
     sales_team = fields.Many2Many(
         'company.company-nereid.user-sales',
         'company', 'nereid_user', 'Sales Team'
     )
 
-Company()
-
 
 class CompanySalesTeam(ModelSQL):
     "Sales Team"
-    _name = 'company.company-nereid.user-sales'
+    __name__ = 'company.company-nereid.user-sales'
     _table = 'company_nereid_sales_team_rel'
-    _description = __doc__
 
     company = fields.Many2One('company.company', 'Company', ondelete='CASCADE',
         required=True, select=True
@@ -470,18 +484,13 @@ class CompanySalesTeam(ModelSQL):
         ondelete='CASCADE', required=True, select=True,
     )
 
-CompanySalesTeam()
 
-
-class NereidReview(ModelSQL, ModelView):
+class NereidReview:
     """
     Nereid Review
     """
-    _name = "nereid.review"
+    __name__ = "nereid.review"
 
     lead = fields.Many2One(
         'sale.opportunity', 'Sale Opportunity Lead'
     )
-
-NereidReview()
-
